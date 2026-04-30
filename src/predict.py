a1b95a8c57c37928ea3a9ee5b026b4ba22cb1bf7
Aishwaryaskulal <nnm22cs013@nmamit.in>
2026-04-30 11:50:18 +0530

    Add real image prediction feature

--- src/predict.py
+++ src/predict.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
 from src.preprocess import load_data
+from PIL import Image
 
 def predict_single():
     (X_train , y_train) , (x_test , y_test) = load_data()
@@ -21,3 +22,22 @@ def predict_single():
     plt.show()
     print(f"True Label : {true_label}")
     print(f"prediction :  {predicted_label}")
+
+def predict_real_image(image_path):
+    img =  Image.open(image_path).convert('L')
+    img = img.resize((28,28))
+    
+    img_array = np.array(img)
+    img_array = 255 - img_array
+    img_array = img_array / 255.0
+    img_array = img_array.reshape(1,28,28,1)
+
+    model = load_model('model/digit_recognizer.keras')
+    prediction = model.predict(img_array)
+    predicted_label = np.argmax(prediction)
+
+    plt.imshow(img_array.reshape(28,28) , cmap='gray')
+    plt.title(f"Predicted: {predicted_label}")
+    plt.axis('off')
+    plt.show()
+    print(f"Predicted Digit : {predicted_label}")
